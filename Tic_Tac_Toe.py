45165d006804e995a4cc396c3a5a83baf4b2bc1b
iAmGroot1 <41311165+iAmGroot1@users.noreply.github.com>
2018-08-07 22:48:44 +0530

    In process to allow the user to play first

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,56 +1,63 @@
-#TIC TAC TOE version 1.2
+#TIC TAC TOE version 1.3
 #author@ Rohan Prasad
 #thing to work on : Implementation of gui and allowing the player to play first 
 def main():
      board={'1': ' ', '2': ' ', '3': ' ', '4': ' ', '5': ' ', '6': ' ', '7': ' ', '8': ' ', '9': ' '}
      turn='X'
-     tic (board,turn,'comp')
+     print("Do you want to start Y/N?")
+     s=input()
+     if(s=="N"):
+        tic (board,turn,'comp')
+     else:
+       tic(board,turn,'player')
      
-def chekSyntax(board,turn,loca):
-    if loca in board:
-           if board[str(loca)]==' ':
-               return True
-           else :
-               print('Coin already placed')
-               return False
-              
-    else :
-           return False
+def chekSyntax(board,turn):
+    for i in range(1,4):
+        print('Please choose where you want to place')
+        printBoard(board)
+        loca=input()
+        if loca in board:
+            if board[str(loca)]==' ':
+               return loca
+            print('Coin already placed')
+            i=i-1
         
+def countCoint(board,turn):
+    count=0
+    for i in range(9):
+        valt=str(i+1)
+        if board[valt]!=' ':
+            count=count+1
+    return count
+    
 def tic (board,turn,toss):
+        count=0
         if toss=='comp':
             print('comp plays')
-            count=0
-            for i in range(9):
-                valt=str(i+1)
-                if board[valt]!=' ':
-                    count=count+1
-
+            count=countCoint(board,turn)
             val=AI(board,turn,count,'comp')
             board[str(val)]='O'
-            printBoard(board)
+            if chekWinner(board):
+                loca=chekSyntax(board,turn)
+                board[str(loca)]='X'
+                tic(board,turn,toss)  
 
-            if chekDraw(board):
-                print("Its a Draw")
-                       
-            elif chekWinner(board):
-                print('Please choose where you want to place')
-                loca=input()
-                for i in range(1,4):
-                    if chekSyntax(board,turn,loca):
-                        board[str(loca)]='X'
-                        break
-                    else:
-                       print('Please choose where you want to place')
-                       printBoard(board)
-                       loca=input()
-                       i=i-1
-                tic(board,turn,toss)
-        
-            else :
-                  print("Computer wins")
-        else:#need to implement when player starts
-            return
+        else :
+            loca=chekSyntax(board,turn)
+            board[str(loca)]=turn
+            count=countCoint(board,turn)
+            val=AI(board,turn,count,'player')
+            board[str(val)]='O'
+            if chekWinner(board):
+                tic(board,turn,toss)  
+        if chekDraw(board):
+            printBoard(board)
+            print("Its a Draw")           
+        else :
+            printBoard(board)
+            print("Computer wins")
+            
+            
 
 def printBoard(board):
       print(board['1'] + '|' + board['2'] + '|' + board['3'])
@@ -88,53 +95,60 @@ def chekWinner(board):
     else:
          return True
         
-        
-
-
-
-
 def AI(board,turn,whoTurn,startTurn):
     play=0
     if startTurn=='comp':
-          if whoTurn==0: play=randm(5)
-          elif whoTurn==2:
-               if board['5']==turn : play=giveDiagonal(findDiagonal(board,oppTurn(turn)))
-               elif board['1']==turn or board['3']==turn or board['7']==turn or board['9']==turn:
-                   val=findDiagonal(board,oppTurn(turn))
-                   play=giveStraight(board,oppTurn(turn),val)
-               else : play=5
-          elif whoTurn==4 :
-               count=countDiagonal(board,oppTurn(turn))
-               result=[chekDiagonal(board,turn),chekStraight(board,turn,'row'),chekStraight(board,turn,'col')]
-               for i in range(3):
-                  if result[i]!=0:
-                     play=result[i]
-                     return play
-               winResult=[chekDiagonal(board,oppTurn(turn)),chekStraight(board,oppTurn(turn),'row'),chekStraight(board,oppTurn(turn),'col')]
-               for i in range(3):
-                      if winResult[i]!=0:
-                        play=winResult[i]
-                        return play
-               if count==2 :
-                   play=giveTwoStaight(board,oppTurn(turn))   
-               else :
-                   val=findDiagonal(board,oppTurn(turn))
-                   play=giveStraight(board,oppTurn(turn),val)
-          else:
-                  winResult=[chekDiagonal(board,oppTurn(turn)),chekStraight(board,oppTurn(turn),'row'),chekStraight(board,oppTurn(turn),'col')]
-                  for i in range(3):
-                      if winResult[i]!=0:
-                        play=winResult[i]
-                  if play==0:                  
-                      result=[chekDiagonal(board,turn),chekStraight(board,turn,'row'),chekStraight(board,turn,'col')]
-                      for i in range(3):
-                        if result[i]!=0:
-                          play=result[i]
-
-      
+        if whoTurn==0: play=randm(5)
+        elif whoTurn==2:
+            if board['5']==turn : play=giveDiagonal(findDiagonal(board,oppTurn(turn)))
+            elif board['1']==turn or board['3']==turn or board['7']==turn or board['9']==turn:
+                val=findDiagonal(board,oppTurn(turn))
+                play=giveStraight(board,oppTurn(turn),val)
+            else : play=5
+        elif whoTurn==4 :
+            count=countDiagonal(board,oppTurn(turn))
+            winReslt=result(board,oppTurn(turn))
+            reslt=result(board,turn)
+            if(winReslt>0): return winReslt
+            elif(reslt>0):  return reslt 
+            if count==2 :
+               play=giveTwoStaight(board,oppTurn(turn))   
+            else :
+               val=findDiagonal(board,oppTurn(turn))
+               play=giveStraight(board,oppTurn(turn),val)
+        else:
+               winReslt=result(board,oppTurn(turn))
+               reslt=result(board,turn)
+               if(winReslt>0): return winReslt
+               elif(reslt>0):  return reslt 
+    else: 
+        if whoTurn==1: 
+            if board['5']==turn: play=randm(5)
+            else : play=5
+        elif whoTurn==3:
+            if board['5']==turn:
+                val=str(giveDiagonal(findDiagonal(board,oppTurn(turn))))
+                if board[val]==turn: play=randm(1)
+                else : return result(board,turn)
+            else:
+                val=str(giveDiagonal(findDiagonal(board,turn)))
+                if board[val]==turn: play=randm(1)
+                elif board['4']==turn: play=1
+                elif board['8']==turn: play=9
+                else : return result(board,turn)
+        else :
+            return result(board,turn)
     return play
-            
-      
+        
+def result(board,turn) :
+    play=0
+    rst=[chekDiagonal(board,turn),chekStraight(board,turn,'row'),chekStraight(board,turn,'col')]
+    for i in range(3):
+        if rst[i]!=0:
+            play=rst[i]
+            break  
+    return play           
+    
 def oppTurn(turn):
     if turn=='X':
         return 'O'
@@ -157,17 +171,12 @@ def findSecondDiagonal(board,turn,valSkip):
         if board[str(i+1)]==turn and i+1!=valSkip:
            return i+1
 
-
-
 def giveDiagonal(num):
-    if num==1:
-        return 9
-    if num==9:
-        return 1
-    if num==3:
-        return 7
-    if num==7:
-        return 3
+    if num==1:return 9
+    if num==9:return 1
+    if num==3:return 7
+    if num==7:return 3
+    return 0
    
 def chekDiagonal(board,turn):
         if board['1']==turn and board['5']==turn and board['9']==' ': return 9
@@ -201,25 +210,17 @@ def chekStraight(board,turn,type):
         
 def giveStraight(board,turn,val):
     if val==9:
-        if board[str(val-1)]==' ' and board[str(val-2)]==' ':
-            return 7
-        if board[str(val-3)]==' ' and board[str(val-6)]==' ':
-            return 3
+        if board[str(val-1)]==' ' and board[str(val-2)]==' ': return 7
+        if board[str(val-3)]==' ' and board[str(val-6)]==' ': return 3
     elif val==7:
-       if board[str(val+1)]==' ' and board[str(val+2)]==' ':
-           return 9
-       if board[str(val-3)]==' ' and board[str(val-6)]==' ':
-           return 1
+       if board[str(val+1)]==' ' and board[str(val+2)]==' ':  return 9
+       if board[str(val-3)]==' ' and board[str(val-6)]==' ':  return 1
     elif val==3:
-        if board[str(val-1)]==' ' and board[str(val-2)]==' ':
-           return 1
-        if board[str(val+3)]==' ' and board[str(val+6)]==' ':
-           return 9
+        if board[str(val-1)]==' ' and board[str(val-2)]==' ': return 1
+        if board[str(val+3)]==' ' and board[str(val+6)]==' ': return 9
     elif val==1:
-        if board[str(val+1)]==' ' and board[str(val+2)]==' ':
-           return 3
-        if board[str(val+3)]==' ' and board[str(val+6)]==' ':
-           return 7
+        if board[str(val+1)]==' ' and board[str(val+2)]==' ': return 3
+        if board[str(val+3)]==' ' and board[str(val+6)]==' ': return 7
     return 0
 
 def countDiagonal(board,turn):
